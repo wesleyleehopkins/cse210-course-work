06fa57bc1544a235ef0bd333725e72559359d512
wesley hopkins <hop16001@byui.edu>
2025-01-15 15:29:28 -0700

    Initialize Sandbox2 project with basic structure and Hello World output

---
+++ sandbox/Sandbox2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sandbox2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World! I made a changCe");
+        Console.WriteLine($""\a)
+    }
+}
