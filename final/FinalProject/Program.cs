cac3a1cc40c5bf8f38efae300c7acd55ef71cee0
wesley hopkins <hop16001@byui.edu>
2025-03-29 07:45:51 -0600

    Implement main menu for Meal Comparison and Tracking software; update UML diagram to reflect new methods and structure

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -3,5 +3,13 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    
+    {
+        int response = 0;
+        while (response != 4) {
+            Console.WriteLine("Please Select an option for your Meal Comparision/tracking software: \n 1. Input Meals \n 2. Compare meal information \n 3.Generate report  \n 4. Quit Program");
+            response = int.Parse(Console.ReadLine());
+
+            if (response = 1)
+        }
+    }
 }
